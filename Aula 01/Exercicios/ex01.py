a78b6b96a1dd24d9c0f30b504b291ee1045d5b3a
Fabio Leal <sousaleal.fabio@gmail.com>
2014-03-12 16:22:53 -0300

    Aula 01

---
+++ Aula 01/Exercicios/ex01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A linha acima nos permite digitar caracteres acentuados no nosso programa.
+#1) Faça um programa que peça dois números inteiros e imprima a soma desses dois números.
+# Lembrem-se que em Python 2 a função input() captura números. A funcao raw_input() captura strings (palavras).
+
+primeiro_numero = input('Digite o primeiro numero: ') #captura o primeiro numero
+segundo_numero = input('Digite o segundo numero: ') #captura o segundo numero
+soma = primeiro_numero + segundo_numero
+
+print 'A soma dos dois numeros eh: ', soma
---
+++ Aula 01/Exercicios/ex05.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#5) Solicite o preço de uma mercadoria e o percentual de desconto oferecido caso a mesma seja paga à vista.
+#Exiba o valor do desconto e o preço a pagar.
+
+valor_mercadoria = float(input("Digite o valor da mercadoria: "))
+percentual_desconto = float(input("Digite o valor do percentual de desconto: "))
+
+valor_do_desconto = valor_mercadoria * (percentual_desconto/100.0)
+preco_a_pagar = valor_mercadoria - valor_do_desconto
+
+print "O valor do desconto é de:", valor_do_desconto
+print "O valor a pagar é de:", preco_a_pagar
